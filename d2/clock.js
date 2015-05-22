a2d01e620ac3419ccb7ae448413c3bf07a29c633
Garrett Simpson <elgaricimo@gmail.com>
2015-05-22 11:29:00 -0700

    Start clock

---
+++ d2/clock.js
@@ -0,0 +1,19 @@
+function Clock () {
+}
+
+Clock.TICK = 5000;
+
+Clock.prototype.printTime = function () {
+
+};
+
+Clock.prototype.run = function () {
+
+};
+
+Clock.prototype._tick = function () {
+
+};
+
+var clock = new Clock();
+clock.run();
