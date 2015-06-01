ed8153015262f03d0e3e39693129a49cabbbaff9
Garrett Simpson <elgaricimo@gmail.com>
2015-06-01 11:31:28 -0700

    Pokemon added to list on refresh

--- w7/app/assets/javascripts/pokedex-1A.js
+++ w7/app/assets/javascripts/pokedex-1A.js
@@ -1,5 +1,17 @@
 Pokedex.RootView.prototype.addPokemonToList = function (pokemon) {
+  var pokeItem = $('<li>').addClass('poke-list-item')
+    .text( pokemon.get("name") +" "+ pokemon.get("poke_type") );
+  this.$pokeList.append(pokeItem);
 };
 
 Pokedex.RootView.prototype.refreshPokemon = function () {
+  var that = this;
+  this.pokes.fetch({
+    success: function(){
+      that.pokes.each(function(pokemon, idx){
+        that.addPokemonToList(pokemon);
+      });
+    }
+  });
+
 };
