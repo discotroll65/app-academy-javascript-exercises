089112b51a46b8fdb375c3a0bede5539b5e43161
Garrett Simpson <elgaricimo@gmail.com>
2015-06-03 11:35:23 -0700

    Setup Journal App

---
+++ w7/JournalApp/app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+end
