5543a72c29dfeb012a737722beeffaa7aa4ab20c
Garrett Simpson <elgaricimo@gmail.com>
2015-06-03 17:20:28 -0700

    Add delete button to show template

--- w7/JournalApp/app/assets/javascripts/views/posts/show_post.js
+++ w7/JournalApp/app/assets/javascripts/views/posts/show_post.js
@@ -1,8 +1,16 @@
 JournalApp.Views.ShowPost = Backbone.View.extend({
   template: JST['posts/show'],
+  events: {
+    "click button.delete": "deletePost"
+  },
 
   initialize: function(){
     this.listenTo(this.model, "sync", this.render);
+  },
+
+  deletePost: function(event) {
+    this.model.destroy();
+    Backbone.history.navigate("#", { trigger: true });
   },
 
   render: function (){
