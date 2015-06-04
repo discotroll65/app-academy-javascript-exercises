84977f65d7748e45c9b08e35f11ad3b9f3b68d61
Garrett Simpson <elgaricimo@gmail.com>
2015-06-03 18:03:07 -0700

    Finish fancy edit

--- w7/JournalApp/app/assets/javascripts/views/posts/show_post.js
+++ w7/JournalApp/app/assets/javascripts/views/posts/show_post.js
@@ -1,11 +1,44 @@
 JournalApp.Views.ShowPost = Backbone.View.extend({
   template: JST['posts/show'],
+  titleEdit: JST["posts/fancyTitleEdit"],
+  bodyEdit: JST["poasts/fancyBodyEdit"],
+
   events: {
-    "click button.delete": "deletePost"
+    "click button.delete": "deletePost",
+    "dblclick .post-body": "editBody",
+    "dblclick .post-title": "editTitle",
+    "submit form": 'updateAttr',
+    "blur .post-title-edit": "updateAttr",
+    "blur .post-body-edit": "updateAttr"
   },
 
   initialize: function(){
     this.listenTo(this.model, "sync", this.render);
+  },
+
+  editBody: function() {
+    var content = this.bodyEdit({ post: this.model });
+    this.$(".post-body").html(content);
+  },
+
+  editTitle: function() {
+    var content = this.titleEdit({ post: this.model });
+    this.$(".post-title").html(content);
+  },
+
+  updateAttr: function(event) {
+    var view = this;
+    event.preventDefault();
+    var form = $(event.currentTarget);
+    var formData = form.serializeJSON()["post"];
+    this.model.save(formData,{
+      success: function(){
+        view.render();
+      },
+      error: function(model, resp){
+        view.$('.errors').html(resp.responseText);
+      }
+    })
   },
 
   deletePost: function(event) {
