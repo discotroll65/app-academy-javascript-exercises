740f8785e1d4504acd4e40f8c3a572705c632833
Garrett Simpson <elgaricimo@gmail.com>
2015-06-04 20:26:05 -0700

    clean up some stuff

--- w7/newsReader/app/assets/javascripts/models/entry.js
+++ w7/newsReader/app/assets/javascripts/models/entry.js
@@ -1,11 +1,9 @@
 NewsReader.Models.Entry = Backbone.Model.extend({
   urlRoot: function () {
-    debugger
     return this.feed.url() + '/entries'
   },
 
   initialize: function (options) {
     this.feed = options.feed;
-
   }
 });
--- w7/newsReader/app/assets/javascripts/views/feeds/feeds_index.js
+++ w7/newsReader/app/assets/javascripts/views/feeds/feeds_index.js
@@ -5,12 +5,11 @@ NewsReader.Views.FeedsIndex = Backbone.CompositeView.extend({
   initialize: function () {
     //below same as this.collection.each(this.addFeedView, this)
 
-    this.collection.each(this.addFeedView.bind(this))
+    this.collection.each(this.addFeedView.bind(this));
     this.listenTo(this.collection, 'add', this.addFeedView);
   },
 
   render: function () {
-    this.checkFeeds()
     var content = this.template();
     this.$el.html(content);
     this.attachSubviews();
@@ -21,10 +20,6 @@ NewsReader.Views.FeedsIndex = Backbone.CompositeView.extend({
     var feedItem = new NewsReader.Views.FeedItem({model: feed});
     this.addSubview('ul', feedItem);
   },
-
-  checkFeeds: function () {
-
-  }
 
 
 
